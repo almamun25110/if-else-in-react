7ff067733afe8ac78bbbe2ef5657c5d8b910ebba
almamun25110 <almamun25110@gmail.com>
2026-01-19 19:56:23 +0600

    Use ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let template = "";
   let [pShow, setpShow] = useState(false);
+  let [status, setStatus] = useState(false);
+  let template = "";
 
   if (pShow) {
     template = (
       <>
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
           onClick={() => {
             setpShow(!pShow);
           }}
@@ -35,6 +36,9 @@ function App() {
   return (
     <div className="App">
       {template}
+      <br/>
+      <button onClick={()=>setStatus(!status)} className="py-2 px-4 shadow-lg cursor-pointer hover:bg-blue-200 rounded-[5px]">{status ? "Hide" : "Show"}</button>
+      {status ? <p>This is a Paragraph</p> : "" }
     </div>
   );
 }
